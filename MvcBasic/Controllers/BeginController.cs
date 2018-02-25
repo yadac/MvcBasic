d4f2ce53e7b989299ae173a8d9b55464fe8a0d99
yosuke adachi <doruji1982@gmail.com>
2018-02-25 18:21:45 +0900

    add begincontroller

---
+++ MvcBasic/Controllers/BeginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBasic.Controllers
+{
+    public class BeginController : Controller
+    {
+        // GET: Begin
+        public ActionResult Index()
+        {
+            return Content("Hello World!!");
+        }
+    }
+}
