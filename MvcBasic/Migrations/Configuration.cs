8a501aa9b0f5cdddd4df4aab8b01ff21409986b7
yosuke adachi <doruji1982@gmail.com>
2018-03-04 23:00:18 +0900

    enable-migration on

---
+++ MvcBasic/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace MvcBasic.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcBasic.Models.MvcBasicContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MvcBasic.Models.MvcBasicContext";
+        }
+
+        protected override void Seed(MvcBasic.Models.MvcBasicContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
