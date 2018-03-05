51600424d4ea16ad170413d682628d68ad5befab
yadac <doruji1982@gmail.com>
2018-03-05 09:22:26 +0900

    add usgin

--- MvcBasic/Models/Article.cs
+++ MvcBasic/Models/Article.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcBasic.Models
 {
