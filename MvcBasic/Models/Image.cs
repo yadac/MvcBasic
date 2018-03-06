c925443d0be3ee38dc218393d39980276fd5ca2f
yadac <doruji1982@gmail.com>
2018-03-06 14:02:49 +0900

    add image model

---
+++ MvcBasic/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcBasic.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Ctype { get; set; }
+        public byte[] Data { get; set; }
+
+    }
+}
