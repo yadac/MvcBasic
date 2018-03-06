fd17bc9a065d2a37a89184daaa8d858875e90453
yadac <doruji1982@gmail.com>
2018-03-06 16:02:36 +0900

    resultempty

---
+++ MvcBasic/Controllers/ResultController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MvcBasic.Controllers
+{
+    public class ResultController : Controller
+    {
+        // GET: Result
+        public ActionResult Index()
+        {
+            return Content("not implemented");
+        }
+
+        public ActionResult Empty()
+        {
+            // 空ページ返却
+            return new EmptyResult();
+        }
+    }
+}
