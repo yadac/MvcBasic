41e6d7ccd003cc51803632c97467155ad45dc393
yadac <doruji1982@gmail.com>
2018-03-06 09:27:31 +0900

    add validation on clientside

--- MvcBasic/Models/Member.cs
+++ MvcBasic/Models/Member.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcBasic.Models
 {
@@ -13,16 +11,21 @@ namespace MvcBasic.Models
     {
         public int Id { get; set; }
         [DisplayName("氏名")]
+        [Required(ErrorMessage = "{0}は必須です")]
+        [RegularExpression("[^a-zA-Z0-9]*", ErrorMessage = "{0}には半角英数字を含めないでください")]
         public string Name { get; set; }
         [DisplayName("メールアドレス")]
+        [EmailAddress(ErrorMessage = "メールアドレスの形式で入力してください")]
         public string Email { get; set; }
         [DisplayName("生年月日")]
+        [Required(ErrorMessage = "{0}は必須です")]
         public DateTime Birth { get; set; }
         [DisplayName("既婚")]
         public bool Married { get; set; }
         [DisplayName("言語")]
         public LanguageEnum Language { get; set; }
         [DisplayName("自己紹介")]
+        [StringLength(100, ErrorMessage = "{0}は{1}文字以内で入力してください")]
         public string Memo { get; set; }
 
     }
