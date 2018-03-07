04ed89e1a6c0eb7479806b084e15bc3111ef7635
yadac <doruji1982@gmail.com>
2018-03-07 09:27:47 +0900

    Filter HandleErrorAttribute

---
+++ MvcBasic/Controllers/FilterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcBasic.Controllers
+{
+    [Authorize]
+    public class FilterController : Controller
+    {
+        [OutputCache(Duration = 600)]
+        // GET: Filter
+        public ActionResult Index()
+        {
+            return Content("not implemented");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Exception()
+        {
+            throw new Exception("Fatal Error");
+        }
+    }
+}
