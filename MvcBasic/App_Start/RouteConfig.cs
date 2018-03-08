f5ec4bcfaaf0a8b458b7ab8a2a7db95817f30fe0
yadac <doruji1982@gmail.com>
2018-03-08 10:04:48 +0900

    change to argumentnamed parameter

--- MvcBasic/App_Start/RouteConfig.cs
+++ MvcBasic/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MvcBasic
@@ -14,9 +10,9 @@ namespace MvcBasic
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Default",
-                "{controller}/{action}/{id}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
