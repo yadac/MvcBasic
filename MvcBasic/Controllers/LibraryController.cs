29aeab446a6490f027ebeafc19823e13bb54e81f
yadac <doruji1982@gmail.com>
2018-03-08 15:09:19 +0900

    img effect jquery

---
+++ MvcBasic/Controllers/LibraryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBasic.Controllers
+{
+    public class LibraryController : Controller
+    {
+        // GET: Library
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
