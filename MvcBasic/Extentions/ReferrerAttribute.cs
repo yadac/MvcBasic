3164435a5f31f360c6b4cb3a7c09cdf060289f6c
yadac <doruji1982@gmail.com>
2018-03-08 09:57:10 +0900

    ActionSelectorAttr

---
+++ MvcBasic/Extentions/ReferrerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace MvcBasic.Extentions
+{
+    public class ReferrerAttribute : ActionMethodSelectorAttribute
+    {
+
+        public bool CanNull { get; private set; }
+
+        public ReferrerAttribute(bool canNull)
+        {
+            CanNull = canNull;
+        }
+
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            var req = controllerContext.HttpContext.Request;
+            var referrer = req.UrlReferrer;
+            if (referrer == null) return CanNull;
+            var refHost = referrer.Host;
+            var curHost = req.Url.Host;
+
+            return refHost.Equals(curHost, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
