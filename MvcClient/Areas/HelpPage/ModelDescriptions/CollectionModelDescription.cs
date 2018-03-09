33cc6114073abf9195956b3cc727e26f73534476
yadac <doruji1982@gmail.com>
2018-03-09 14:28:32 +0900

    Add WebAPIProject

---
+++ MvcClient/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MvcClient.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
