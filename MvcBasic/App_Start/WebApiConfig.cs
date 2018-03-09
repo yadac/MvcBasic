1be8c096cab75d224bbbef8b046cf37b262c5f6f
yadac <doruji1982@gmail.com>
2018-03-09 16:48:16 +0900

    multiple startup project connectionstring configuration file

---
+++ MvcBasic/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+
+namespace MvcBasic.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API の設定およびサービス
+
+            // Web API ルート
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+
+}
+
---
+++ MvcBasic/Areas/API/Controllers/ArticlesController.cs
@@ -0,0 +1,19 @@
+﻿using MvcBasic.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace MvcBasic.Areas.API.Controllers
+{
+    public class ArticlesController : ApiController
+    {
+        private readonly MvcBasicContext db = new MvcBasicContext();
+
+        public IEnumerable<Article> GetArticlesByCategory(CategoryEnum id)
+        {
+            var articles = db.Articles.Where(a => a.Category == id).OrderBy(a => a.Published);
+            return articles;
+        }
+
+    }
+}
