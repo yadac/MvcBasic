73d6a7480f92a23b68f42117f430f9baa95038a3
yadac <doruji1982@gmail.com>
2018-03-09 10:18:41 +0900

    ajax

---
+++ MvcBasic/Controllers/AjaxController.cs
@@ -0,0 +1,36 @@
+﻿using MvcBasic.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MvcBasic.Controllers
+{
+    public class AjaxController : Controller
+    {
+
+        private readonly MvcBasicContext _db = new MvcBasicContext();
+
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return Content("not imple");
+        }
+
+        public ActionResult AjaxLink()
+        {
+            var list = EnumHelper.GetSelectList(typeof(CategoryEnum));
+            return View(list);
+        }
+
+        public ActionResult AjaxSearch(CategoryEnum id)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var articles = _db.Articles.Where(a => a.Category == id).OrderBy(a => a.Published);
+                return PartialView("_AjaxSearch", articles);
+            }
+
+            return View("AjaxLink");
+        }
+    }
+}
