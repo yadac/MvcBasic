45fdd6828d1d278c37c9070390a2c40bead2d6e8
yosuke adachi <doruji1982@gmail.com>
2018-03-11 20:26:42 +0900

    add ut

---
+++ MvcBasicTests/Extentions/ReferrerAttributeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MvcBasic.Extentions;
+
+namespace MvcBasicTests.Extentions
+{
+    [TestClass()]
+    public class ReferrerAttributeTests
+    {
+        [TestMethod()]
+        public void IsValidForRequestTest()
+        {
+            // Arrange
+            var uri1 = new Uri("http://www.wings.msn.to/Test/Previous");
+            var uri2 = new Uri("http://yahoo.co.jp/Member/Create");
+
+            var context = new Mock<ControllerContext>();
+            context.SetupGet<Uri>(c => c.HttpContext.Request.UrlReferrer).Returns(uri1);
+            context.SetupGet<Uri>(c => c.HttpContext.Request.Url).Returns(uri2);
+
+            var info = new Mock<MethodInfo>();
+            var attr = new ReferrerAttribute(true);
+
+            // Act
+            var result = attr.IsValidForRequest(context.Object, info.Object);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
